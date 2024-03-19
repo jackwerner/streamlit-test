4051b6d63cabdba599ecf1fee6f33df32fe6f028
jackwerner <jckwern@aol.com>
2024-03-19 10:34:38 -0400

    Update poptherm.py

--- poptherm.py
+++ poptherm.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 import numpy as np
 import streamlit as st
-
+print(st.secrets['TEST'])
 def get_lat_lng(address):
     api_key = st.secrets['GOOGLE_KEY']
     base_url = 'https://maps.googleapis.com/maps/api/geocode/json'
